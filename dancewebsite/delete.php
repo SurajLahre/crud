84af5b02417834e9ed801cd62c22b00526730f93
SurajLahre <107786386+SurajLahre@users.noreply.github.com>
2023-07-25 15:51:33 +0530

    Update delete.php
    delete.php apdated

--- dancewebsite/delete.php
+++ dancewebsite/delete.php
@@ -1,25 +1,15 @@
 <?php
 include "dbconnect.php";
+if (isset($_GET['del_id'])){
+  $uid=$_GET['del_id'];
 
-if(isset($_GET['id']))
-{
-  $id = senitize_input($_GET['id']);
-
-  // parse sql
-  $res =mysqli_query($conn, "DELETE FROM `user` WHERE `id`= $id");
-
-  $stmt = $conn->prepare($res);
-  if($stmt){
-    $stmt->bind_param('i',$id);
-    // echo "succesully delted !";
-$stmt->execute();
-$stmt->close();
-  }
-  else{
-
-    echo "error faild to prepare statement";
-
+  $sql ="DELETE FROM `user` WHERE id=$uid";
+  $result =mysqli_query($conn, $sql);
+  if($result){
+    Header('location:index.php');
+  }else{
+    die(mysqli_connect_error($conn));
   }
 }
-$conn->close();
+
 ?>
